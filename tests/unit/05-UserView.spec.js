7e71f68ad48730704cce8a80fb75d22f64ab5202
Roberto Zagnoni <r.zagnoni@gmail.com>
2019-07-24 15:33:05 +0200

    tutorial add test 05

---
+++ tests/unit/05-UserView.spec.js
@@ -0,0 +1,40 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import UserView from '@/views/UserView'
+import VUserSearchForm from '@/components/VUserSearchForm'
+import VUserProfile from '@/components/VUserProfile'
+import initialState from '@/store/state'
+import userFixture from './fixtures/user'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('UserView', () => {
+  let state
+
+  const build = () => {
+    const wrapper = shallowMount(UserView, {
+      localVue,
+      store: new Vuex.Store({ state })
+    })
+
+    return {
+      wrapper,
+      userSearchForm: () => wrapper.find(VUserSearchForm),
+      userProfile: () => wrapper.find(VUserProfile)
+    }
+  }
+
+  beforeEach(() => {
+    state = { ...initialState }
+  })
+
+  it('passes a binded user prop to user profile component', () => {
+    // arrange
+    state.user = userFixture
+    const { userProfile } = build()
+
+    // assert
+    expect(userProfile().vm.user).toBe(state.user)
+  })
+})
