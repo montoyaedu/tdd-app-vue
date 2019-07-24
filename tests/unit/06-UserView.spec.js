566cfc90027a8e85b1189e49cf322be0c6707e0c
Roberto Zagnoni <r.zagnoni@gmail.com>
2019-07-24 16:12:38 +0200

    tutorial test 06

---
+++ tests/unit/06-UserView.spec.js
@@ -0,0 +1,51 @@
+jest.mock('@/store/actions')
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import UserView from '@/views/UserView'
+import VUserSearchForm from '@/components/VUserSearchForm'
+import VUserProfile from '@/components/VUserProfile'
+import initialState from '@/store/state'
+import actions from '@/store/actions'
+import userFixture from './fixtures/user'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('UserView', () => {
+  let state
+
+  const build = () => {
+    const wrapper = shallowMount(UserView, {
+      localVue,
+      store: new Vuex.Store({
+        state,
+        actions,
+      })
+    })
+
+    return {
+      wrapper,
+      userSearchForm: () => wrapper.find(VUserSearchForm),
+      userProfile: () => wrapper.find(VUserProfile)
+    }
+  }
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    state = { ...initialState }
+  })
+
+  it('searches for a user when received "submitted"', () => {
+    // arrange
+    const expectedUser = 'kuroski'
+    const { userSearchForm } = build()
+
+    // act
+    userSearchForm().vm.$emit('submitted', expectedUser)
+
+    // assert
+    expect(actions.SEARCH_USER).toHaveBeenCalled()
+    expect(actions.SEARCH_USER.mock.calls[0][1]).toEqual({ username: expectedUser })
+  })
+
+})
